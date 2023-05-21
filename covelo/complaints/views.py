eb7f983380457affc73762d65b0cf9ce2f70cb1c
liemgumball <liem1762001@gmail.com>
2023-05-21 19:19:05 +0700

    api create complaints

--- covelo/complaints/views.py
+++ covelo/complaints/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+class CreateRentalComplaint(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RentalComplaintSerializer
+    
+class CreateBicycleComplaint(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = BicycleComplaintSerializer
+    
+class CreateStationComplaint(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = StationComplaintSerializer
+    
--- covelo/covelo/settings.py
+++ covelo/covelo/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'covelo.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    # 'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     #     'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'covelo',
